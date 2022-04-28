4bb96b2d17282d558a7848c6382e8d0db1c4e29b
Semenikhin Alexander <mu97@mail.ru>
2022-04-28 05:36:19 +0500

    preloat header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         </div> */}
       </div>
       <div className='gpt3__header-image'>
-        <img src={ai} alt="ai" />
+        <img rel="preload" src={ai} alt="ai" />
       </div>
     </header>
   )
