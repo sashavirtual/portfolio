0b3e9b93d2d5f4087bd7a289a493dc81f3b7dc09
Semenikhin Alexander <mu97@mail.ru>
2022-04-28 07:09:44 +0500

    init

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         </div> */}
       </div>
       <div className='gpt3__header-image'>
-        <img rel="preload" src={ai} alt="ai" />
+        <img src={ai} alt="ai" />
       </div>
     </header>
   )
