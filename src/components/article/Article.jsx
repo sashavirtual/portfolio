934c1d49eb87fbef22fe78d480d7d18ede27332a
Semenikhin Alexander <mu97@mail.ru>
2022-06-08 08:57:19 +0500

    visual improvement

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './article.css'
 
 const Article = ({ imgUrl, description, title, website }) => {
+  let nonProjectArr = [
+    'https://www.codewars.com/users/Aleksandr%20Semenikhin',
+    'https://github.com/sashavirtual?tab=repositories'
+  ]
   return (
     <div className='sasha__projects-container_article'>
       <div className='sasha__projects-container_article-image'>
@@ -12,7 +16,7 @@ const Article = ({ imgUrl, description, title, website }) => {
         <p>{description}</p>
       </div>
       <div className="sasha__projects-container_article-paragraph">
-        <a href={website} target="_blank">View the project</a>
+        <a href={website} target="_blank">{nonProjectArr.includes(website) ? 'View the web-site' : 'View the project'}</a>
       </div>
     </div>
   )
